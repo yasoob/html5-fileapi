e721c606c1095b6873c853d08d3492e1cf1dfa56
M.Yasoob Ullah Khalid ☺ <yasoob.khld@gmail.com>
2016-01-02 16:10:22 +0500

    updated the code

--- js/main.js
+++ js/main.js
@@ -18,20 +18,3 @@ zone.event('send', function (files) {
 })
 
 zone.multiple(true);
-
-
-function handleFileSelect(evt) {
-var files = evt.target.files; // FileList object
-
-// files is a FileList of File objects. List some properties.
-var output = [];
-for (var i = 0, f; f = files[i]; i++) {
-	output.push('<ul class="item"><li><b>Name:</b> ', decodeURIComponent(escape(f.name)), '</li><li><b>Type:</b> ', f.type || 'n/a', '</li><li><b>Size:</b> ',
-		f.size, ' bytes, </li><li><b>last modified:</b> ',
-		f.lastModifiedDate ? f.lastModifiedDate.toLocaleDateString() : 'n/a',
-		'</li></ul>');
-}
-document.getElementById('list').innerHTML = '<h4>Information</h4><ul class="main">' + output.join('') + '</ul>';
-}
-
-document.getElementById('files').addEventListener('change', handleFileSelect, false);
